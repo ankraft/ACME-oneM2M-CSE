1963fae19f02d46bd5a14b23ae7314f6ee16e4f2
ankraft <an.kraft@googlemail.com>
2020-06-17 22:24:59 +0200

    Ignore cache when building image

--- tools/Docker/buildDocker.sh
+++ tools/Docker/buildDocker.sh
@@ -2,6 +2,6 @@
 DOCKERHUBID=ankraft
 
 cd ../..
-docker build -t acme-onem2m-cse -f tools/Docker/Dockerfile .
+docker build --no-cache -t acme-onem2m-cse -f tools/Docker/Dockerfile .
 docker tag acme-onem2m-cse $DOCKERHUBID/acme-onem2m-cse
 docker push $DOCKERHUBID/acme-onem2m-cse
