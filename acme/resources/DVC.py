0edafc29561ec7354d6f669de1597f645a0cc7a1
ankraft <an.kraft@googlemail.com>
2020-08-08 13:58:02 +0200

    Improved ina/dis behaviour

--- acme/resources/DVC.py
+++ acme/resources/DVC.py
@@ -21,7 +21,7 @@ attributePolicies = addPolicy(mgmtObjAttributePolicies, dvcPolicies)
 
 class DVC(MgmtObj):
 
-	def __init__(self, jsn: dict = None, pi: str = None, create: bool = False) -> None:
+	def __init__(self, jsn:dict=None, pi:str=None, create:bool=False) -> None:
 		self.resourceAttributePolicies = dvcPolicies	# only the resource type's own policies
 		super().__init__(jsn, pi, mgd=T.DVC, create=create, attributePolicies=attributePolicies)
 
@@ -30,4 +30,26 @@ class DVC(MgmtObj):
 			self.setAttribute('att', False, overwrite=False)
 			self.setAttribute('cas', {	"acn" : "unknown", "sus" : 0 }, overwrite=False)
 			self.setAttribute('cus', False, overwrite=False)
+			self.setAttribute('ena', True, overwrite=True)	# always True
+			self.setAttribute('dis', True, overwrite=True)	# always True
+
+
+	def validate(self, originator: str = None, create: bool = False) -> Tuple[bool, int, str]:
+		if (res := super().validate(originator, create))[0] == False:
+			return res
+		self.setAttribute('ena', True, overwrite=True)	# always set (back) to True
+		self.setAttribute('dis', True, overwrite=True)	# always set (back) to True
+		return True, C.rcOK, None
+
+
+	def update(self, jsn:dict=None, originator:str=None) -> Tuple[bool, int, str]:
+		# Check for ena & dis updates 
+		if jsn is not None and self.tpe in jsn:
+			ena = Utils.findXPath(jsn, 'm2m:dvc/ena')
+			dis = Utils.findXPath(jsn, 'm2m:dvc/dis')
+			if ena is not None and dis is not None and ena and dis:
+				return False, C.rcBadRequest, 'both ena and dis updated to True is not allowed'
+
+		return super().update(jsn, originator)
+
 
