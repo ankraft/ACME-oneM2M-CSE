39028b7d3ea3d92044e6b84882a60d633e0294a3
ankraft <an.kraft@gmail.com>
2020-10-12 10:27:09 +0200

    Added first stubs for PCU

---
+++ acme/resources/PCH.py
@@ -0,0 +1,39 @@
+#
+#	PCH.py
+#
+#	(c) 2020 by Andreas Kraft
+#	License: BSD 3-Clause License. See the LICENSE file for further details.
+#
+#	ResourceType: PollingChannel
+#
+
+from Constants import Constants as C
+from Types import ResourceTypes as T, Result
+from Validator import constructPolicy, addPolicy
+import Utils
+from .Resource import *
+from .AnnounceableResource import AnnounceableResource
+
+
+# Attribute policies for this resource are constructed during startup of the CSE
+attributePolicies = constructPolicy([ 
+	'ty', 'ri', 'rn', 'pi', 'acpi', 'ct', 'lt', 'et', 'lbl', 'daci', 
+])
+reqPolicies = constructPolicy([
+])
+attributePolicies = addPolicy(attributePolicies, reqPolicies)
+
+
+class PCH(Resource):
+
+	def __init__(self, jsn:dict=None, pi:str=None, create:bool=False) -> None:
+		super().__init__(T.PCH, jsn, pi, create=create, attributePolicies=attributePolicies)
+
+		# TODO attribute requestAggregation
+
+
+	# Enable check for allowed sub-resources
+	def canHaveChild(self, resource:Resource) -> bool:
+		return super()._canHaveChild(resource, [ T.PCH_PCU ])
+
+
---
+++ acme/resources/PCH_PCU.py
@@ -0,0 +1,65 @@
+#
+#	PCH_PCU.py
+#
+#	(c) 2020 by Andreas Kraft
+#	License: BSD 3-Clause License. See the LICENSE file for further details.
+#
+#	ResourceType: PollingChannelURI for PollingChannel
+#
+
+from flask import Request
+from Constants import Constants as C
+from Types import ResourceTypes as T, ResponseCode as RC
+import CSE, Utils
+from .Resource import *
+from Logging import Logging
+
+
+class PCH_PCU(Resource):
+
+	def __init__(self, jsn:dict=None, pi:str=None, create:bool=False) -> None:
+		super().__init__(T.PCH_PCU, jsn, pi, create=create, inheritACP=True, readOnly=True, rn='ol', isVirtual=True)
+
+
+
+
+
+
+
+
+
+# TODO
+
+
+
+
+	# # Enable check for allowed sub-resources
+	# def canHaveChild(self, resource:Resource) -> bool:
+	# 	return super()._canHaveChild(resource, [])
+
+
+	# def handleRetrieveRequest(self, request:Request=None, id:str=None, originator:str=None) -> Result:
+	# 	""" Handle a RETRIEVE request. Return resource """
+	# 	Logging.logDebug('Retrieving oldest FCI from FCNT')
+	# 	if (r := self._getOldest()) is None:
+	# 		return Result(rsc=RC.notFound, dbg='no instance for <oldest>')
+	# 	return Result(resource=r)
+
+
+	# def handleCreateRequest(self, request:Request, id:str, originator:str, ct:str, ty:int) -> Result:
+	# 	""" Handle a CREATE request. Fail with error code. """
+	# 	return Result(rsc=RC.operationNotAllowed, dbg='operation not allowed for <oldest> resource type')
+
+
+	# def handleUpdateRequest(self, request:Request, id:str, originator:str, ct:str) -> Result:
+	# 	""" Handle a UPDATE request. Fail with error code. """
+	# 	return Result(rsc=RC.operationNotAllowed, dbg='operation not allowed for <oldest> resource type')
+
+
+	# def handleDeleteRequest(self, request:Request, id:str, originator:str) -> Result:
+	# 	""" Handle a DELETE request. Delete the latest resource. """
+	# 	Logging.logDebug('Deleting oldest FCI from FCNT')
+	# 	if (r := self._getOldest()) is None:
+	# 		return Result(rsc=RC.notFound, dbg='no instance for <oldest>')
+	# 	return CSE.dispatcher.deleteResource(r, originator, withDeregistration=True)
+
