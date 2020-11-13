5d062cc390e3358fa6cac81c92fcab7f7f2ffdd3
ankraft <an.kraft@gmail.com>
2020-11-13 09:05:14 +0100

    Documentation update

--- acme.py
+++ acme.py
@@ -34,8 +34,8 @@ def parseArgs() -> argparse.Namespace:
 	groupRemoteCSE.add_argument('--no-remote-cse', action='store_false', dest='remotecseenabled', default=None, help='disable remote CSE connections')
 
 	groupRemoteCSE = parser.add_mutually_exclusive_group()
-	groupRemoteCSE.add_argument('--statistics', action='store_true', dest='statisticsenabled', default=None, help='enable collecting CSE statics')
-	groupRemoteCSE.add_argument('--no-statistics', action='store_false', dest='statisticsenabled', default=None, help='disable collecting CSE statics')
+	groupRemoteCSE.add_argument('--statistics', action='store_true', dest='statisticsenabled', default=None, help='enable collecting CSE statistics')
+	groupRemoteCSE.add_argument('--no-statistics', action='store_false', dest='statisticsenabled', default=None, help='disable collecting CSE statistics')
 
 	groupRemoteCSE = parser.add_mutually_exclusive_group()
 	groupRemoteCSE.add_argument('--validation', action='store_true', dest='validationenabled', default=None, help='enable attributes and arguments validation')
@@ -47,9 +47,9 @@ def parseArgs() -> argparse.Namespace:
 
 	parser.add_argument('--db-reset', action='store_true', dest='dbreset', default=None, help='reset the DB when starting the CSE')
 	parser.add_argument('--db-storage', action='store', dest='dbstoragemode', default=None, choices=[ 'memory', 'disk' ], type=str.lower, help='specify the DB´s storage mode')
-	parser.add_argument('--http-address', action='store', dest='httpaddress', default=None, help='specify the http server address')
+	parser.add_argument('--http-address', action='store', dest='httpaddress', metavar='<server URL>', default=None, help='specify the CSE\'s http server URL')
 	parser.add_argument('--import-directory', action='store', dest='importdirectory', default=None, metavar='<directory>', help='specify the import directory')
-	parser.add_argument('--network-interface', action='store', dest='listenif', default=None, help='specify the network interface to bind to')
+	parser.add_argument('--network-interface', action='store', dest='listenif', metavar='<ip address>', default=None, help='specify the network interface/IP address to bind to')
 	parser.add_argument('--log-level', action='store', dest='loglevel', default=None, choices=[ 'info', 'error', 'warn', 'debug', 'off'], type=str.lower, help='set the log level, or turn logging off')
 	
 	return parser.parse_args()
