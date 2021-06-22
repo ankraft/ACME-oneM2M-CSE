229e3366423ef96fcd4cb1091aefd3a729c8305f
ankraft <an.kraft@gmail.com>
2021-06-22 18:04:21 +0200

    Further functionality. Also split into client and handler

--- acme/MQTTClient.py
+++ acme/MQTTClient.py
@@ -10,8 +10,10 @@
 from __future__ import annotations
 import time
 from dataclasses import dataclass
+from typing import Callable
 from Logging import Logging as L
 from Configuration import Configuration
+from Types import ContentSerializationType
 from helpers.BackgroundWorker import BackgroundWorkerPool, BackgroundWorker
 import CSE, Utils
 
@@ -24,22 +26,78 @@ class MQTTTopic:
 	topic:str			= None
 	mid:int				= None
 	isSubscribed:bool	= False
+	callback:Callable	= None
+	callbackArgs:dict 	= None
+
+
+# TODO split MQTThandler stuff. Only two callbacks for connected and disconnected to handle subscriptions.
+
+class MQTTHandler(object):
+
+	def onConnect(self, mqttClient:MQTTClient):
+		# Subscribe to general requests
+		mqttClient.subscribeTopic(f'{mqttClient.topicPrefix}/oneM2M/req/+/{mqttClient.clientName}/cbor', self._requestCallback, serialization=ContentSerializationType.CBOR)
+		mqttClient.subscribeTopic(f'{mqttClient.topicPrefix}/oneM2M/req/+/{mqttClient.clientName}/json', self._requestCallback, serialization=ContentSerializationType.JSON)
+
+		# TODO Subscribe to register requests
+
+		# TODO Subscribe to responses
+
+	def onDisconnect(self, mqttClient:MQTTClient):
+		pass
+
+	#
+	#	Various request, register and response callbacks
+	#
+
+	def _requestCallback(self, mqttClient:MQTTClient, topic:str, data:str, args):
+		L.logDebug(f'REQUEST {topic}, {data}, {args}')
+		mqttClient.publish('test', f'{topic}, {data}, {args}')
+
+
+
+##############################################################################
+
 
 class MQTTClient(object):
 
-	def __init__(self) -> None:
-		self.mqttClient:mqtt.Client 			= None
-		self.actor:BackgroundWorker 			= None
-		self.enable								= Configuration.get('mqtt.enable')
-		self.brokerAddress						= Configuration.get('mqtt.address')
-		self.brokerPort							= Configuration.get('mqtt.port')
-		self.keepalive							= Configuration.get('mqtt.keepalive')
-		self.bindIF								= Configuration.get('mqtt.bindIF')
-		self.isStopped							= True
-		self.isConnected						= False
-		self.subscribedTopics:list[MQTTTopic]	= []
-		self.clientName							= f'C::{Utils.getIdFromOriginator(CSE.cseCsi)}'
+	def __init__(self, mqttHandler:MQTTHandler=MQTTHandler()) -> None:
+		self.mqttHandler:MQTTHandler				= mqttHandler
+		self.mqttClient:mqtt.Client 				= None
+		self.actor:BackgroundWorker 				= None
+		self.enable									= Configuration.get('mqtt.enable')
+		self.brokerAddress							= Configuration.get('mqtt.address')
+		self.brokerPort								= Configuration.get('mqtt.port')
+		self.keepalive								= Configuration.get('mqtt.keepalive')
+		self.bindIF									= Configuration.get('mqtt.bindIF')
+		self.username:str							= None  # TODO config
+		self.password:str 							= None  # TODO config
+		self.isStopped								= True
+		self.isConnected							= False
+		self.clientName								= idToMQTT(CSE.cseCsi)
+		self.topicPrefix:str						= ''	# TODO config value
+		self.subscribedTopics:dict[str, MQTTTopic]	= {}
 		L.isInfo and  L.log('MQTT client initialized')
+
+	
+	def shutdown(self) -> bool:
+		"""	Shutting down the MQTT client.
+		"""
+		self.isStopped = True
+		if self.enable and self.mqttClient is not None:
+			if self.isConnected:
+				# Unsubscribe from all topics
+				for t in self.subscribedTopics.values():
+					self.unsubscribeTopic(t)
+				# wait a moment for all unsubscribe ACKs to arrive
+				while len(self.subscribedTopics) > 0:
+					time.sleep(0.1)
+				# Then disconnect. The actor is stoped implicitly
+				self.mqttClient.disconnect()
+				self.actor = None
+
+		L.isInfo and L.log('MQTT client shut down')
+		return True
 
 
 	def run(self) -> None:
@@ -69,9 +127,8 @@ class MQTTClient(object):
 			CSE.shutdown()
 			return
 
-		# Actually start the actor to sun the MQTT client as a thread
-		self.actor = BackgroundWorkerPool.newActor(self._mqttActor, name='MQTTClient')
-		self.actor.start()
+		# Actually start the actor to run the MQTT client as a thread
+		self.actor = BackgroundWorkerPool.newActor(self._mqttActor, name='MQTTClient').start()
 
 
 	def _mqttActor(self) -> bool:
@@ -79,40 +136,25 @@ class MQTTClient(object):
 		"""
 		self.isStopped = False	
 		if L.isInfo: L.log('MQTT: client started')
-		self.mqttClient.loop_forever()	# Will return when disconnect() is called
+		while not self.isStopped:
+			self.mqttClient.loop_forever()	# Will return when disconnect() is called
 		return True
 	
 
-	def shutdown(self) -> bool:
-		"""	Shutting down the MQTT client.
-		"""
-		if self.enable and self.mqttClient is not None:
-			if self.isConnected:
-				# Unsubscribe from all topics
-				for t in self.subscribedTopics:
-					self.unsubscribeTopic(t)
-				# wait a moment for all unsubscribe ACKs to arrive
-				while len(self.subscribedTopics) > 0:
-					time.sleep(0.1)
-				# Then disconnect. The actor is stoped implicitly
-				self.mqttClient.disconnect()
-				self.actor = None
-
-		self.isStopped = True
-		L.isInfo and L.log('MQTT client shut down')
-		return True
-	
-
-	def subscribeTopic(self, topic:str) -> None:
+	def subscribeTopic(self, topic:str, callback:Callable=None, **kwargs) -> None:
 		"""	Add a MQTT topic to subscribe to. Add this topic afterwards
 			to the list of subscribed to topics.
 		"""
 		if self.mqttClient is None or not self.isConnected:
 			L.logErr('MQTT: Client missing or not initialized')
 			return
+		if topic in self.subscribedTopics:
+			L.isWarn and L.logWarn(f'MQTT: topic already subscribed: {topic}')
+			return
+
 		if (r := self.mqttClient.subscribe(topic))[0] == 0:
-			t = MQTTTopic(topic = topic, mid=r[1])
-			self.subscribedTopics.append(t)
+			t = MQTTTopic(topic = topic, mid=r[1], callback=callback, callbackArgs=kwargs)
+			self.subscribedTopics[topic] = t
 		else:
 			L.logErr(f'MQTT: cannot subscribe: {r[0]}')
 			pass
@@ -124,25 +166,40 @@ class MQTTClient(object):
 			it is searched for in the list of MQTTTopics.
 		"""
 		if isinstance(topic, MQTTTopic):
+			if topic.topic not in self.subscribedTopics:
+				L.isWarn and L.logWarn(f'MQTT: unknown topic: {topic.topic}')
+				return
 			if (r := self.mqttClient.unsubscribe(topic.topic))[0] == 0:
 				topic.mid = r[1]
 			else:
 				L.logErr(f'MQTT: cannot unsubscribe: {r[0]}')
 				return
 
-		else:	# if topic is just the name we need to se
-			for t in self.subscribedTopics:
-				if t.topic == topic and t.isSubscribed:
-					if (r := self.mqttClient.unsubscribe(t.topic))[0] == 0:
-						t.mid = r[1]
-					else:
-						L.logErr(f'MQTT: cannot unsubscribe: {r[0]}')
-						return
+		else:	# if topic is just the name we need to subscribe to
+			if topic not in self.subscribedTopics:
+				L.isWarn and L.logWarn(f'MQTT: unknown topic: {topic}')
+				return
+			t = self.subscribedTopics[topic]
+			if t.isSubscribed:
+				if (r := self.mqttClient.unsubscribe(t.topic))[0] == 0:
+					t.mid = r[1]
+				else:
+					L.logErr(f'MQTT: cannot unsubscribe: {r[0]}')
+					return
+			else:
+				L.isWarn and L.logWarn(f'MQTT: topic not subscribed: {topic}')
+
 		# topic is removed in _onUnsubscribe() callback
+	
+
+
+	def publish(self, topic:str, data:bytes):
+		# TODO doc
+		self.mqttClient.publish(topic, data)
 
 
 	#
-	#	MQTT callbacks
+	#	MQTT/paho callbacks
 	#
 
 	def _onConnect(self, client, userdata, flags, rc):
@@ -151,8 +208,8 @@ class MQTTClient(object):
 		L.isDebug and L.logDebug(f'MQTT: Connected with result code: {rc}')
 		if rc == 0:
 			self.isConnected = True
-			self.subscribeTopic('test')
-			self.subscribeTopic('test2')
+			if self.mqttHandler is not None:
+				self.mqttHandler.onConnect(self)
 		else:
 			CSE.shutdown()
 
@@ -164,6 +221,8 @@ class MQTTClient(object):
 		if rc == 0:
 			self.isConnected = False
 			self.subscribedTopics.clear()
+			if self.mqttHandler is not None:
+				self.mqttHandler.onDisconnect(self)
 
 
 	def _onLog(self, client, userdata, level, buf):
@@ -181,8 +240,7 @@ class MQTTClient(object):
 
 	def _onSubscribe(self, client, userdata, mid, granted_qos):
 		# TODO doc, error check when not connected, not subscribed
-		L.isDebug and L.logDebug(f'{client}, {userdata}, {mid}, {granted_qos}')
-		for t in self.subscribedTopics:
+		for t in self.subscribedTopics.values():
 			if t.mid == mid:
 				t.isSubscribed = True
 				break
@@ -190,15 +248,35 @@ class MQTTClient(object):
 
 	def _onUnsubscribe(self, client, userdata, mid):
 		# TODO doc, error check when not connected, not subscribed
-		L.isDebug and L.logDebug(f'{client}, {userdata}, {mid}')
-		for t in list(self.subscribedTopics):
+		for t in self.subscribedTopics.values():
 			if t.mid == mid:
-				self.subscribedTopics.remove(t)
+				del self.subscribedTopics[t.topic]
 				break
 
 
 	def _onMessage(self, client, userdata, message:mqtt.MQTTMessage):
-		# TODO doc, error check when not connected, not subscribed
+		"""	Handle a received message. Forward it to the apropriate handler callback (in a Thread)
+		"""
+		L.isDebug and L.logDebug(f'MQTT: received topic:{message.topic}, payload:{message.payload}')
+		for t in self.subscribedTopics.keys():
+			if Utils.simpleMatch(message.topic, t):
+				topic = self.subscribedTopics[t]
+				if topic.callback is not None:
+					# Run actual request handling in a thread
+					BackgroundWorkerPool.newActor(topic.callback, name=f'mid_{message.mid}').start(	mqttClient=self,
+																									topic=message.topic,
+																									data=message.payload, 
+																									args=topic.callbackArgs)
+				break
 
-		L.isDebug and L.logDebug(f'{client}, {userdata}, {message.topic}, {message.payload}')
-		self.mqttClient.publish('bla', str(Utils.utcTime()))
+
+
+
+##############################################################################
+#
+#	Utility functions
+#
+
+def idToMQTT(id:str, isCSE:bool=True) -> id:
+	return f'{"C:" if isCSE else "A:"}{id.replace("/", ":")}'
+
