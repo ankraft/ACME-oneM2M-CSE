9c4c2f66c2ec0e411e0e2467d93fa1382136c936
ankraft <an.kraft@gmail.com>
2021-07-27 18:34:19 +0200

    Prepare for release

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / 'README.md').read_text()
 
 setup(
     name='ACME-oneM2M-CSE',
@@ -7,7 +14,17 @@ setup(
     author='Andreas Kraft',
     author_email='an.kraft@gmail.com',
     description='An open source CSE Middleware for Education',
-    packages=find_packages(),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    #packages=find_packages(),
+    packages=[ 'acme', 'apps', 'webui' ],
+	exclude=("tests",),
+    include_package_data=True,
 	install_requires=[
 		'cbor2',
 		'flask',
@@ -19,7 +36,9 @@ setup(
 		'tinydb',
 		#'package1 @ git+https://github.com/CITGuru/PyInquirer.git@9d598a53fd17a9bc42efff33183cd2d141a5c949'
 	],
+    entry_points={
+        "console_scripts": [
+            "acme-cse=acme.__main__:main",
+        ]
+    },
 )
-	#dependency_links=[
-		#'git+https://github.com/CITGuru/PyInquirer.git@9d598a53fd17a9bc42efff33183cd2d141a5c949' 
-    #]
