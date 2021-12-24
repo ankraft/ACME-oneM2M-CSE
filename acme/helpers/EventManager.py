a7a54edde81db5afc929b1ba60e44cab0e218924
ankraft <an.kraft@googlemail.com>
2021-12-24 23:57:10 +0100

    Added hasHandler(), improved documentation

--- acme/helpers/EventManager.py
+++ acme/helpers/EventManager.py
@@ -45,25 +45,28 @@ class Event(list):	# type:ignore[type-arg]
 	Attention: Since the parent class is *list* `isInstance(obj, list)` will yield True.
 	"""
 
-	def __init__(self, runInBackground:bool=True, manager:EventManager=None):
+	def __init__(self, runInBackground:bool = True, manager:EventManager = None):
 		self.runInBackground = runInBackground
 		self.manager = manager
 
 
 	def __call__(self, *args:Any, **kwargs:Any) -> None:
+		"""	Handle calling an event. This calls any of the registered callback functions for this
+			event. If the event was created with `runInBackground` as True, then the callbacks
+			are called sequentially (not individually!) as a thread.
+		"""
 
 		def _callThread(*args:Any, **kwargs:Any) -> None:
-			"""	Call all attached function for this event object.
+			"""	Call all registered function for this event object. Pass on any argument
 			"""
 			for function in self:
 				function(*args, **kwargs)
-
 
 		if not self.manager._running:
 			return
 		if self.runInBackground:
 			# Call the handlers in a thread so that we don't block everything
-			thread = threading.Thread(target=_callThread, args=args, kwargs=kwargs)
+			thread = threading.Thread(target = _callThread, args = args, kwargs = kwargs)
 			thread.setDaemon(True)		# Make the thread a daemon of the main thread
 			thread.start()
 			Utils.renameCurrentThread(thread=thread)
@@ -93,8 +96,15 @@ class EventManager(object):
 
 	#########################################################################
 
-	def addEvent(self, name:str, runInBackground:bool=True) -> Event:
+	def addEvent(self, name:str, runInBackground:bool = True) -> Event:
 		"""	Create and add a new event.
+
+			Args:
+				name: Name of the event.
+				runInBackgroun: (optional, default = True) Execute the callbacks in a thread.
+			
+			Returns:
+				The created Event
 		"""
 		if not hasattr(self, name):
 			setattr(self, name, Event(runInBackground=runInBackground, manager=self))
@@ -102,7 +112,10 @@ class EventManager(object):
 
 
 	def removeEvent(self, name:str) -> None:
-		"""	Remove an event by name.
+		"""	Remove an event by its name.
+
+			Args:
+				name: The name of the event to remove
 		"""
 		if hasattr(self, name):
 			delattr(self, name)
@@ -118,17 +131,39 @@ class EventManager(object):
 
 	def hasEvent(self, name:str) -> bool:
 		"""	Check whether an event exists.
+
+			Args:
+				name: Name of the event to check.
 		"""
 		return name in self.__dict__
 
 
 	def addHandler(self, event:Event|list[Event], func:Callable) -> None:		# type:ignore[type-arg]
 		"""	Add a new event handler for an `event` or a list of events.
+
+			Args:
+				event: Either a single Event or a list of Event objects
+				func: The function callback to call when the event is raised.
 		"""
 		list(map(lambda e: e.append(func), [event] if isinstance(event, Event) else event))
+	
+
+	def hasHandler(self, event:Event|list[Event], func:Callable) -> bool:
+		"""	Test whether one or more events have a specific handler assigned.
+
+			Args:
+				event: Either a single Event or a list of Event objects
+				func: The function callback to call when the event is raised.
+		"""
+		l = [ event ] if isinstance(event, Event) else event
+		return any([ e for e in l if func in e ])
 
 
 	def removeHandler(self, event:Event|list[Event], func:Callable) -> None:	# type:ignore[type-arg]
 		"""	Remove an event handler from an `event` or a list of events.
+
+			Args:
+				event: Either a single Event or a list of Event objects
+				func: The function callback to remove from the even t.
 		"""
 		list(map(lambda e: e.remove(func), [event] if isinstance(event, Event) else event))
