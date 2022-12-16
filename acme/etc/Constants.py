7c1ac5398629f05076d4435544caa434e78ab393
Andreas Kraft <an.kraft@gmail.com>
2022-12-16 16:43:05 +0100

    Prepare 0.11.1

--- acme/etc/Constants.py
+++ acme/etc/Constants.py
@@ -10,7 +10,7 @@
 class Constants(object):
 	""" Various CSE and oneM2M constants """
 
-	version	= '0.12.0-dev'
+	version	= '0.11.1'
 	"""	ACME's release version """
 
 	textLogo = '[dim][[/dim][red][i]ACME[/i][/red][dim]][/dim]'
