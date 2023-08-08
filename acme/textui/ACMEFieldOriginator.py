496a4e1f2d672ff337af027c64eed54644da7529
ankraft <an.kraft@gmail.com>
2023-08-08 15:15:15 +0200

    Updated input field

--- acme/textui/ACMEFieldOriginator.py
+++ acme/textui/ACMEFieldOriginator.py
@@ -6,6 +6,7 @@
 #
 
 from __future__ import annotations
+from typing import Optional
 from textual.app import ComposeResult
 from textual.containers import Container, Vertical
 from textual.widgets import Input, Label
@@ -13,17 +14,11 @@ from textual.suggester import SuggestFromList
 from textual.validation import Function
 from textual import on
 
-# TODO This may has to be turned into a more generic field class
 
-idFieldOriginator = 'field-originator'
-
-def validateOriginator(value: str) -> bool:
-	return value is not None and len(value) > 1 and value.startswith(('C', 'S', '/'))
-
-class ACMEFieldOriginator(Container):
+class ACMEField(Container):
 	
 	DEFAULT_CSS = """
-	ACMEFieldOriginator {
+	ACMEField {
 		width: 1fr;
 		height: 4;
 		layout: horizontal;
@@ -33,18 +28,18 @@ class ACMEFieldOriginator(Container):
 		margin: 1 1 1 1;
 	}
 
-	#field-originator-label {
+	#field-label {
 		height: 1fr;
 		content-align: left middle;
 		align: left middle;
 	}
 
-	#field-originator-input {
+	#field-input {
 		height: 1fr;
 		width: 1fr;
 	}
 
-	#field-originator-pretty {
+	#field-pretty {
 		height: 1fr;
 		width: 1fr;
 		margin-left: 1;
@@ -52,18 +47,21 @@ class ACMEFieldOriginator(Container):
 	}
 	"""
 
-	def __init__(self, originator:str, suggestions:list[str] = []) -> None:
+	def __init__(self, label:str = 'a label',
+					   suggestions:list[str] = [],
+					   placeholder:str = '',
+					   validators:Function = None,
+					   id:str = None) -> None:
 		# TODO list of originators as a suggestion
-		super().__init__(id = idFieldOriginator)
-		self.originator = originator
+		super().__init__(id = id)
 		self.suggestions = suggestions
-		self.label = Label('[b]Originator[/b] ', id = 'field-originator-label')
+		self.label = Label(f'[b]{label}[/b] ', id = f'field-label')
 		self.input = Input(str(self.suggestions), 
-						   placeholder = 'Originator',
+						   placeholder = placeholder,
 						   suggester = SuggestFromList(self.suggestions),
-						   validators = Function(validateOriginator, 'Wrong originator format: Must start with "C", "S" or "/", and have length > 1.'),
-						   id = 'field-originator-input')
-		self.msg = Label('jjj', id = 'field-originator-pretty')
+						   validators = validators,
+						   id = 'field-input')
+		self.msg = Label('jjj', id = 'field-pretty')
 
 	def compose(self) -> ComposeResult:
 		yield self.label
@@ -82,9 +80,100 @@ class ACMEFieldOriginator(Container):
 			self.originator = event.value
 
 
+
+
+
+
+# TODO This may has to be turned into a more generic field class
+
+idFieldOriginator = 'field-originator'
+
+def validateOriginator(value: str) -> bool:
+	return value is not None and len(value) > 1 and value.startswith(('C', 'S', '/')) and not set(value) & set(' \t\n')
+
+class ACMEFieldOriginator(ACMEField):
+	def __init__(self, originator:str, suggestions:list[str] = []) -> None:
+		super().__init__(label = 'Originator',
+		   				 suggestions = suggestions,
+						 placeholder = 'Originator',
+						 validators = Function(validateOriginator, 
+			     							   'Wrong originator format: Must start with "C", "S" or "/", contain now white spaces, and have length > 1.')
+						)
+		self.originator = originator
+		self.suggestions = suggestions
+
 	def update(self, originator:str, suggestions:list[str] = []) -> None:
 		self.originator = originator
 		self.suggestions = suggestions
 		self.input.value = originator
 		self.input.suggester = SuggestFromList(self.suggestions)
+
+
+# class ACMEFieldOriginator(Container):
+	
+# 	DEFAULT_CSS = """
+# 	ACMEFieldOriginator {
+# 		width: 1fr;
+# 		height: 4;
+# 		layout: horizontal;
+# 		overflow: hidden hidden;
+# 		# background: red;
+# 		content-align: left middle;
+# 		margin: 1 1 1 1;
+# 	}
+
+# 	#field-originator-label {
+# 		height: 1fr;
+# 		content-align: left middle;
+# 		align: left middle;
+# 	}
+
+# 	#field-originator-input {
+# 		height: 1fr;
+# 		width: 1fr;
+# 	}
+
+# 	#field-originator-pretty {
+# 		height: 1fr;
+# 		width: 1fr;
+# 		margin-left: 1;
+# 		color: red;
+# 	}
+# 	"""
+
+# 	def __init__(self, originator:str, suggestions:list[str] = []) -> None:
+# 		# TODO list of originators as a suggestion
+# 		super().__init__(id = idFieldOriginator)
+# 		self.originator = originator
+# 		self.suggestions = suggestions
+# 		self.label = Label('[b]Originator[/b] ', id = 'field-originator-label')
+# 		self.input = Input(str(self.suggestions), 
+# 						   placeholder = 'Originator',
+# 						   suggester = SuggestFromList(self.suggestions),
+# 						   validators = Function(validateOriginator, 'Wrong originator format: Must start with "C", "S" or "/", and have length > 1.'),
+# 						   id = 'field-originator-input')
+# 		self.msg = Label('jjj', id = 'field-originator-pretty')
+
+# 	def compose(self) -> ComposeResult:
+# 		yield self.label
+# 		with Vertical():
+# 			yield self.input
+# 			yield self.msg
+
+
+# 	@on(Input.Changed)
+# 	def show_invalid_reasons(self, event: Input.Changed) -> None:
+# 		# Updating the UI to show the reasons why validation failed
+# 		if not event.validation_result.is_valid:  
+# 			self.msg.update(event.validation_result.failure_descriptions[0])
+# 		else:
+# 			self.msg.update('')
+# 			self.originator = event.value
+
+
+# 	def update(self, originator:str, suggestions:list[str] = []) -> None:
+# 		self.originator = originator
+# 		self.suggestions = suggestions
+# 		self.input.value = originator
+# 		self.input.suggester = SuggestFromList(self.suggestions)
 
