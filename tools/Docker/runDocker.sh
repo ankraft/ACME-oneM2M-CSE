21c49553ac5d09ee5cac3d62e3035aef046590ad
ankraft <an.kraft@gmail.com>
2024-05-03 00:17:15 +0200

    Add instructions for mapping and env var

--- tools/Docker/runDocker.sh
+++ tools/Docker/runDocker.sh
@@ -6,9 +6,11 @@ DOCKERHUBID=ankraft
 hostIP=`ipconfig getifaddr en1`
 
 # run ACME in Docker
+#
+# Map directory to data directory: -v /path/to/data:/data
+# Set environment variable for docker host IP: -e DOCKER_HOST_IP=`ifconfig en0 | awk '$1 == "inet" {print $2}'`
 if [ -n "$hostIP" ]; then
 	docker run -e COLUMNS="`tput cols`" -e LINES="`tput lines`" -it -p 8080:8080 --add-host="localhost:$hostIP" --rm --name acme-onem2m-cse $DOCKERHUBID/acme-onem2m-cse
 else
 	docker run -e COLUMNS="`tput cols`" -e LINES="`tput lines`" -it -p 8080:8080  --rm --name acme-onem2m-cse $DOCKERHUBID/acme-onem2m-cse
 fi
-
