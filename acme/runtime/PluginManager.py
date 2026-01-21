d4aea7665ac538837098407cc4ea36db854861f6
ankraft <an.kraft@gmail.com>
2026-01-21 16:14:37 +0100

    Improved plugin manager

--- acme/runtime/PluginManager.py
+++ acme/runtime/PluginManager.py
@@ -14,41 +14,52 @@ from ..runtime import CSE
 from ..runtime.Logging import Logging as L
 
 
-class PluginManager:
+class PluginManager(PM):
 
 	def __init__(self) -> None:
-		self.pluginManager:PM = PM()
 		"""	Runtime instance of the `PluginManager`. """
+		super().__init__()
 
 		# Add handler for restart event
 		CSE.event.addHandler(CSE.event.cseReset, self.restart)		# type: ignore
 
+		L.isDebug and L.logDebug('Initializing PluginManager')
+
 		# Load plugins, configure, validate and start them
-		self.pluginManager.loadPlugins(directory=f'{Configuration.moduleDirectory}/plugins', 
-								 	   packagePath='acme.plugins', 
-								  	   disabledPlugins=Configuration.cse_operation_plugins_disabledPlugins, 
-								  	   replace=Configuration.cse_operation_plugins_replace)	# Load system plugins
+		self.loadPlugins(directory=f'{Configuration.moduleDirectory}/plugins', 
+						 packagePath='acme.plugins', 
+						 disabledPlugins=Configuration.cse_operation_plugins_disabledPlugins, 
+						 replace=Configuration.cse_operation_plugins_replace)	# Load system plugins
+		self.loadPlugins(directory=f'{Configuration.moduleDirectory}/runtime/plugins', 
+						 packagePath='acme.runtime', 
+						 disabledPlugins=Configuration.cse_operation_plugins_disabledPlugins, 
+						 replace=Configuration.cse_operation_plugins_replace)	# Load system runtime plugins
+		self.loadPlugins(directory=f'{Configuration.moduleDirectory}/services/plugins', 
+						 packagePath='acme.services', 
+						 disabledPlugins=Configuration.cse_operation_plugins_disabledPlugins, 
+						 replace=Configuration.cse_operation_plugins_replace)	# Load system servicesplugins
+
 		try:
 			# Load plugins from working directory's plugins directory, if it exists
-			self.pluginManager.loadPlugins(directory=f'{Configuration.baseDirectory}/plugins', 
-								  		   packagePath='acme.plugins', 
-										   disabledPlugins=Configuration.cse_operation_plugins_disabledPlugins, 
-								  		   replace=Configuration.cse_operation_plugins_replace)		# Load working directory plugins
+			self.loadPlugins(directory=f'{Configuration.baseDirectory}/plugins', 
+							 packagePath='acme.plugins', 
+							 disabledPlugins=Configuration.cse_operation_plugins_disabledPlugins, 
+							 replace=Configuration.cse_operation_plugins_replace)		# Load working directory plugins
 		except NotADirectoryError:
 			# Ignore if the directory does not exist
 			pass
 
-		L.isInfo and L.log(f'Loaded plugins: {", ".join(self.pluginManager.plugins.keys())}')
-		self.pluginManager.configurePlugins(None, Configuration)
-		self.pluginManager.validatePlugins(None, Configuration)
-		self.pluginManager.startPlugins()
+		L.isInfo and L.log(f'Loaded plugins: {", ".join(self.plugins.keys())}')
+		self.configurePlugins(None, Configuration)
+		self.validatePlugins(None, Configuration)
+		self.startPlugins()
 		L.isInfo and L.log('Plugins configured and started')
 
 
 	def restart(self, _: str) -> None:
 		"""	Restart the PluginManager service.
 		"""
-		self.pluginManager.restartPlugins()
+		self.restartPlugins()
 		L.isDebug and L.logDebug('Plugins restarted')
 
 		
@@ -57,6 +68,6 @@ class PluginManager:
 		"""
 		# Shutdown plugins
 		L.isInfo and L.log('Shutting down and unloading plugins')
-		self.pluginManager.unloadPlugins()	# This implicitly stops the plugins as well
+		self.unloadPlugins()	# This implicitly stops the plugins as well
 		L.isInfo and L.log('Plugins stopped and unloaded')
 		return True
