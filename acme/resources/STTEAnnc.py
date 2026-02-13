6c9f56dfcbff001ffe3bcfbcf7e939c39ff99b2a
ankraft <an.kraft@gmail.com>
2026-02-13 14:10:51 +0100

    Added State Announced resource

---
+++ acme/resources/STTEAnnc.py
@@ -0,0 +1,14 @@
+#
+#	STTEAnnc.py
+#
+#	(c) 2022 by Andreas Kraft
+#	License: BSD 3-Clause License. See the LICENSE file for further details.
+#
+#	STTE : Announceable variant
+#
+
+from .AnnouncedResource import AnnouncedResource
+
+class STTEAnnc(AnnouncedResource):
+
+	pass
